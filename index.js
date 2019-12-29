dfeb579069b3ec81e1e37cbb72662d5d4d6bd287
Jeff Sutherland <jeffdsuth@gmail.com>
2019-12-29 09:07:53 +0700

    Extract routers by resource

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ const users = require('./fixtures/users');
 const emails = require('./fixtures/emails');
 const app = express();
 
-const getUsersRoute = (req, res) => res.send(users);
+const getUsersRoute = (req, res) => {
+  res.send(users);
+}
+const usersRouter = express.Router();
+usersRouter.get('/users', getUsersRoute)
+usersRouter.get('/users/:id', getUsersRoute)
+
 const getEmailsRoute = (req, res) => res.send(emails);
+const emailsRouter = express.Router();
+emailsRouter.get('/emails', getEmailsRoute)
+
 const routeNotFound = (req, res) => res.end(`You asked for ${req.method} ${req.url}`)
 
-const router = express.Router();
-router.get('/users', getUsersRoute)
-router.get('/emails', getEmailsRoute)
-
-app.use(router);
+app.use(usersRouter);
+app.use(emailsRouter);
 app.use(routeNotFound); // FALLBACK
 
 app.listen(3000);
