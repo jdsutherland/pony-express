9b1e05146518cda79985518b675e4ba527704d76
Jeff Sutherland <jeffdsuth@gmail.com>
2019-12-29 10:36:01 +0700

    Add update email

--- routes/emails.js
+++ routes/emails.js
@@ -19,10 +19,19 @@ const createEmailRoute = async (req, res) => {
   res.send(newEmail);
 }
 
+const updateEmailRoute = async (req, res) => {
+  const body = await readBody(req);
+  let email = emails.find(u => u.id === req.params.id)
+  Object.assign(email, JSON.parse(body))
+  res.status(200);
+  res.send(email);
+}
+
 const emailsRouter = express.Router();
 
 emailsRouter.get('/', getEmailsRoute)
 emailsRouter.get('/:id', getEmailRoute)
 emailsRouter.post('/', createEmailRoute)
+emailsRouter.patch('/:id', updateEmailRoute)
 
 module.exports = emailsRouter;
