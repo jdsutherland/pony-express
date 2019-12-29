c6c23e8ffe4ddec15def5e0fdb0f71b959f8395f
Jeff Sutherland <jeffdsuth@gmail.com>
2019-12-29 09:19:50 +0700

    Extract to routers to separate files

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-const http = require('http');
 const express = require('express');
-
-const users = require('./fixtures/users');
-const emails = require('./fixtures/emails');
+const usersRouter = require('./routes/users');
+const emailsRouter = require('./routes/emails');
 const app = express();
-
-const getUsersRoute = (req, res) => {
-  res.send(users);
-}
-const usersRouter = express.Router();
-usersRouter.get('/', getUsersRoute)
-usersRouter.get('/:id', getUsersRoute)
-
-const getEmailsRoute = (req, res) => res.send(emails);
-const emailsRouter = express.Router();
-emailsRouter.get('/', getEmailsRoute)
 
 const routeNotFound = (req, res) => res.end(`You asked for ${req.method} ${req.url}`)
 
---
+++ routes/emails.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const emails = require('../fixtures/emails');
+
+const getEmailsRoute = (req, res) => res.send(emails);
+const getEmailRoute = (req, res) => {
+  const email = emails.find(u => u.id === req.params.id)
+  res.send(email);
+}
+
+const emailsRouter = express.Router();
+
+emailsRouter.get('/', getEmailsRoute)
+emailsRouter.get('/:id', getEmailRoute)
+
+module.exports = emailsRouter;
