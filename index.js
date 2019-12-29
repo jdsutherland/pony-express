0a43513438ff12ea8e680790c6698a4a531fa8a9
Jeff Sutherland <jeffdsuth@gmail.com>
2019-12-29 08:43:54 +0700

    Refactor router pattern
    1. Extract each route handler to fn
    2. Create Routes map of endpoints to handlers
    3. In the server, get the handler and || to default

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const express = require('express');
 
 const users = require('./fixtures/users');
 const emails = require('./fixtures/emails');
-
 const app = express();
+
+const getUsersRoute = (req, res) => res.send(users);
+const getEmailsRoute = (req, res) => res.send(emails);
+
+const ROUTES = new Map([
+  ['GET /users', getUsersRoute],
+  ['GET /emails', getEmailsRoute],
+]);
 
 app.use((req, res) => {
   const route = `${req.method} ${req.url}`;
+  const handler = ROUTES.get(route) || res.end(`You asked for ${route}`)
 
-  if (route === 'GET /users') {
-    res.send(users)
-  } else if (route === 'GET /emails') {
-    res.send(emails)
-  }
-  res.end(`You asked for ${route}`)
-
+  handler(req, res);
 })
 
 app.listen(3000);
