b53a84ecddf0ad262376db296e5e30f06e6f0699
Jeff Sutherland <jeffdsuth@gmail.com>
2019-12-29 10:27:15 +0700

    Demo request bodies w telnet

--- routes/emails.js
+++ routes/emails.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const readBody = require('../lib/read-body');
+const generateId = require('../lib/generate-id');
 const emails = require('../fixtures/emails');
 
 const getEmailsRoute = (req, res) => res.send(emails);
+
 const getEmailRoute = (req, res) => {
   const email = emails.find(u => u.id === req.params.id)
   res.send(email);
+}
+
+const createEmailRoute = async (req, res) => {
+  const body = await readBody(req);
+
+  const newEmail = { ...JSON.parse(body), id: generateId() }
+  emails.push(newEmail);
+  res.status(201);
+  res.send(newEmail);
 }
 
 const emailsRouter = express.Router();
 
 emailsRouter.get('/', getEmailsRoute)
 emailsRouter.get('/:id', getEmailRoute)
+emailsRouter.post('/', createEmailRoute)
 
 module.exports = emailsRouter;
